bb0596d6303f2edf5b10d5664bcee84283b98be3
steve <steve.nguyen2013@gmail.com>
2022-04-09 13:31:55 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const farmAnimals = 'cow horse sheep pig chicken';
 
+
+const [moo,neigh,baa,oink,cluck] = farmAnimals.split(" ");
+const [bessie,, dolly, babe, little]= farmAnimals.split(" ");
+const [blackAndWhite,, black,pink] = farmAnimals.split(" ");
+
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+
+let [red,orange,yellow,green,blue,indigo,violet] = [...colors]
+let [r,o,y,g,b,,v]= [...colors]
+let [,,,,,indg]=[...colors]
 
 const muppet = {
   muppetName: 'Miss Piggy',
@@ -24,25 +34,11 @@ const nestedMuppet = {
   nestedJob: 'Host of The Muppet Show',
   nestedPartner: 'Miss Piggy'
 };
-
-// Strings
-
-// 1. Use destructuring to assign appropriate variables based on the sounds animals make.
-
-// 2. Bolt the horse wandered off, so just give us four animals, and let's name them bessie, dolly, babe, and little.
-
-// 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of blackAndWhite, black, and pink.
-
-// Arrays
-
-// 4. Use destructuring to assign appropriate variables using the color names.
-
-// 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
-
-// 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
-
+const {muppetName="Miss Piggy",color="pink",song="Never Before, Never Again",job= "Cast member of The Muppet Show",partner= "Kermit"} = {muppet};
 // Objects
 
-// 7. Use destructuring to assign all variables using the keys as the variable names
+//const {nestedJob,nestedPartner,album:{theMuppetMovie: {song2,song4}}}= nestedMuppet
 
-// 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+//both ways work. ^^ or vv
+
+const {nestedMuppet: {nestedJob="Host of the Muppet Show",nestedPartner="Miss Piggy",album: { theMuppetMovie: {song2="Moving Right Along",song4="I Hope That Something Better Comes Along"}}}}= {nestedMuppet}
